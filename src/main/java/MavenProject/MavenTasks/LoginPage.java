9591e09fd8da0aeec791d18de6b2c7343fa860ae
Praveen5314 <praveen5314@gmail.com>
2018-04-26 12:53:45 +0530

    Mo

---
+++ src/main/java/MavenProject/MavenTasks/LoginPage.java
@@ -0,0 +1,13 @@
+package MavenProject.MavenTasks;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginPage {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Elcot\\eclipse-workspace\\test\\Driver\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.facebook.com/");	
+		System.out.println("facebook");
+	}
+}
