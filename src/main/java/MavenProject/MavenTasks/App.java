252b494a17748e80bad5801fd016d21e56a64eb6
Praveen5314 <praveen5314@gmail.com>
2018-04-26 11:44:46 +0530

    Initial Commit

---
+++ src/main/java/MavenProject/MavenTasks/App.java
@@ -0,0 +1,13 @@
+package MavenProject.MavenTasks;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
